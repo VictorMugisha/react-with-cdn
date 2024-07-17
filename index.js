244631aca945c671016094e657ff32f3a7b9f172
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-17 10:45:06 +0200

    This is simple example of how to work with React and CDN links

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const customPage = (
+    <div>
+        <h3>This should be the header</h3>
+        <ul>
+            <li>This is the first list item</li>
+            <li>This is the second list item</li>
+            <li>This is the third list item</li>
+            <li>This is the forth list item</li>
+            <li>This is the fith list item</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(
+    customPage,
+    document.getElementById('root')
+)
